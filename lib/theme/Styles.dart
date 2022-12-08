fa5cc2e0e9049e427f3d3c37f8ab215c63251e3f
阮皓 <52927295@qq.com>
2022-12-08 17:32:57 +0800

    add theme

---
+++ lib/theme/Styles.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const _textSizeLarge = 25.0;
+  static const _textSizeDefault = 16.0;
+
+  static final appTheme = new ThemeData(
+    hintColor: Colors.white,
+  );
+}
